79a0af7dd898d0d31e80874d5f276da0b1c8a6e9
Aliaksandr Liashevich <aliaksandr.liashevich@gmail.com>
2019-01-27 22:35:16 +0300

    Realize methods 'addHiveSection', 'updateHiveSection', 'deleteHiveSection' in HiveSectionService. Realize HiveSectionFormComponent. Add buttons 'Back', 'Save', 'Delete', 'Restore', 'Purge'.

--- KatlaSportNg/src/app/app-routing.module.ts
+++ KatlaSportNg/src/app/app-routing.module.ts
@@ -25,7 +25,8 @@ const routes: Routes = [
   { path: 'hive', component: HiveFormComponent },
   { path: 'hive/:id', component: HiveFormComponent },
   { path: 'hive/:id/sections', component: HiveSectionListComponent },
-  { path: 'section/:id', component: HiveSectionFormComponent },
+  { path: 'section', component: HiveSectionFormComponent },
+  { path: 'section/:id', component: HiveSectionFormComponent }
 ];
 
 @NgModule({
